ebc4328d1d7110aa3f32a465c6339d9e91ab5d55
j <j@w3reality.com>
2018-10-08 12:29:30 +0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ class Var2EsmPlugin {
             // https://webpack.js.org/contribute/plugin-patterns/
             // https://github.com/webpack/docs/wiki/how-to-write-a-plugin#a-simple-example
             // console.log('======== debug ========');
-            // for (const [key, value] of Object.entries(compilation.assets)) {
-            //     let len = value._value.length;
-            //     console.log('key len:', key, len); // this.outputFile nnnn
-            //     console.log('starts with:', value._value.substring(0, 64));
-            //     console.log('ends with:', value._value.substring(len-64, len));
-            //     value._value = `${this.libraryObjName}........`;
+            // for (const [key, asset] of Object.entries(compilation.assets)) {
+            //     let src = asset.source();
+            //     let len = src.length;
+            //     console.log('key, len:', key, len); // this.outputFile nnnn
+            //     console.log('starts with:', src.substring(0, 64));
+            //     console.log('ends with:', src.substring(len-64, len));
             // }
             // console.log('======== ========');
 
